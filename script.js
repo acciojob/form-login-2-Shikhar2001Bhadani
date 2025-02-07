59a86e6df124b78cd205859ca31c7b7bfbb71c5a
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-07 18:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.getElementById("submitbtn").addEventListener("click",function(){
+	let name = document.getElementById("fname").value;
+	let lastname = document.getElementById("lname").value;
+	let number = document.getElementById("phno").value;
+	let email = document.getElementById("email").value;
+
+	alert(`First Name: ${name} Last Name: ${lastname} Phone Number: ${number} Email ID: ${email}`);
+
+});
